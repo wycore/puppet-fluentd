d24781fcad1486cb7ed3dae41a2e8296ad633714
Max Wilhelm <max.wilhelm@wywy.com>
2015-12-01 15:11:05 +0100

    added parameter validation for fluentd::filter

--- manifests/filter.pp
+++ manifests/filter.pp
@@ -6,6 +6,15 @@ define fluentd::filter (
   $pattern  = '',
   $config   = {},
 ) {
+
+  # parameter validation
+  if ! ($ensure in [ 'present', 'absent' ]) {
+    fail('ensure parameter must be present or absent')
+  }
+  validate_integer($priority)
+  validate_string($pattern)
+  validate_hash($config)
+
   fluentd::config::file { "filter-${title}":
     ensure   => $ensure,
     priority => $priority,
