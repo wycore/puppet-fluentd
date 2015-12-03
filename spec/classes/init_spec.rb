1cc05f5b1b104deed5ee72bf11da64a4b5122b95
Max Wilhelm <max.wilhelm@wywy.com>
2015-12-03 17:53:01 +0100

    WIP: added first init.pp test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
-describe 'fluentd' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('fluentd') }
+describe 'fluentd', :type => :class do
+  let(:facts) {
+    {
+      :osfamily        => 'Debian',
+      :lsbdistid       => 'Ubuntu',
+      :operatingsystem => 'Ubuntu',
+      :lsbdistcodename => 'precise',
+    }
+  }
+
+  describe 'when called with no parameters on Ubuntu' do
+    it {
+      should contain_class('fluentd::repo')
+      should contain_class('fluentd::install')
+      should contain_class('fluentd::config')
+      should contain_class('fluentd::service')
+    }
   end
 end
