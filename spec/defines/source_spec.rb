df6c698931c36329729d75dba24f233e20f45909
Max Wilhelm <max.wilhelm@wywy.com>
2015-12-04 13:11:08 +0100

    added source_spec

---
+++ spec/defines/source_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe 'fluentd::source' do
+  let(:facts) {
+    {
+      :osfamily        => 'Debian',
+      :lsbdistid       => 'Ubuntu',
+      :operatingsystem => 'Ubuntu',
+      :lsbdistcodename => 'precise',
+    }
+  }
+  let(:pre_condition) { 'include fluentd' }
+
+  context 'basic source' do
+    let(:title) { 'test' }
+    let(:params) do
+      {
+          priority: '10',
+          config: {
+            'type'   => 'tail',
+            'format' => 'json',
+            'path'   => '/var/log/test-application/*.json',
+            'tag'    => 'application.test'
+          }
+      }
+    end
+
+    it do
+      is_expected.to contain_file('/etc/td-agent/conf.d/10-source-test.conf').
+        with_content(/<source>/).
+        with_content(/type tail/).
+        with_content(/format json/).
+        with_content(/path \/var\/log\/test-application\/\*.json/).
+        with_content(/tag application.test/).
+        with_content(/<\/source>/)
+    end
+  end
+end
