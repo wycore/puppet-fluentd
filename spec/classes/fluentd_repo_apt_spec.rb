ea344aa9ad20f05e018be5c5ed4d7f029f221f1f
Max Wilhelm <max.wilhelm@wywy.com>
2015-12-04 12:29:06 +0100

    added fluentd_repo_apt_spec

---
+++ spec/classes/fluentd_repo_apt_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe 'fluentd::repo::apt', :type => :class do
+  let(:facts) {
+    {
+      :osfamily        => 'Debian',
+      :lsbdistid       => 'Ubuntu',
+      :operatingsystem => 'Ubuntu',
+      :lsbdistcodename => 'precise',
+    }
+  }
+
+  describe 'when called with no parameters on Ubuntu' do
+    it {
+      should contain_class('apt')
+      should contain_apt__source('treasure-data').with({
+        'ensure'   => 'present',
+        'location' => 'http://packages.treasuredata.com/2/ubuntu/precise',
+        'release'  => 'precise',
+        'repos'    => 'contrib',
+        'key'      => {
+          'id'     => '1093DB45A12E206F',
+          'source' => 'https://packages.treasuredata.com/GPG-KEY-td-agent'
+        }
+      })
+    }
+  end
+end
