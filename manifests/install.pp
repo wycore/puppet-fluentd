490868d6b1b8cc6d58f61aa1a7da9d349173a5a3
Coto Cisternas <cotocisternas@gmail.com>
2016-01-20 00:40:21 -0300

    require => Exec['apt_update'] before install

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,6 +6,7 @@ class fluentd::install inherits fluentd {
     ensure          => $::fluentd::package_ensure,
     name            => $::fluentd::package_name,
     install_options => $::fluentd::package_install_options,
+    require         => Exec['apt_update'],
   }
 
 }
