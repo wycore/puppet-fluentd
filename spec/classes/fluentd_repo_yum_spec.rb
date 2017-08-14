b34c7dc4e4f99d37b7f07f4776d69d8fcd8efabc
Max Wilhelm <max.wilhelm@wywy.com>
2017-08-14 17:56:33 +0200

    use rspec-puppet-facts for fluentd_repo_yum_spec

--- spec/classes/fluentd_repo_yum_spec.rb
+++ spec/classes/fluentd_repo_yum_spec.rb
@@ -1,27 +1,33 @@
 require 'spec_helper'
 
 describe 'fluentd::repo::yum', :type => :class do
-  let(:facts) {
-    {
-      :osfamily => 'RedHat',
-    }
-  }
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let :facts do
+        facts
+      end
 
-  describe 'when called with no parameters on RedHat' do
-    it {
-      should contain_yumrepo('treasure-data').with({
-        'ensure'   => 'present',
-        'baseurl'  => 'https://packages.treasuredata.com/2/redhat/$releasever/$basearch',
-        'descr'    => 'TreasureData',
-        'enabled'  => '1',
-        'gpgcheck' => '1'
-      }).that_notifies('Exec[add GPG key]')
 
-      should contain_exec('add GPG key').with({
-        'command'     => 'rpm --import https://packages.treasuredata.com/GPG-KEY-td-agent',
-        'path'        => '/bin:/usr/bin/',
-        'refreshonly' => 'true'
-      })
-    }
+      describe 'when called with no parameters' do
+        case facts[:osfamily]
+        when 'RedHat'
+          it {
+            should contain_yumrepo('treasure-data').with({
+              'ensure'   => 'present',
+              'baseurl'  => 'https://packages.treasuredata.com/2/redhat/$releasever/$basearch',
+              'descr'    => 'TreasureData',
+              'enabled'  => '1',
+              'gpgcheck' => '1'
+            }).that_notifies('Exec[add GPG key]')
+
+            should contain_exec('add GPG key').with({
+              'command'     => 'rpm --import https://packages.treasuredata.com/GPG-KEY-td-agent',
+              'path'        => '/bin:/usr/bin/',
+              'refreshonly' => 'true'
+            })
+          }
+        end
+      end
+    end
   end
 end
