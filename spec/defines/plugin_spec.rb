613a28ccaf29fa28aa0b5318c4afa15699fbfe15
Max Wilhelm <max.wilhelm@wywy.com>
2017-08-17 13:34:03 +0200

    use rspec-puppet-facts for plugin_spec

--- spec/defines/plugin_spec.rb
+++ spec/defines/plugin_spec.rb
@@ -1,52 +1,51 @@
 require 'spec_helper'
 
 describe 'fluentd::plugin' do
-  let(:facts) {
-    {
-      :osfamily        => 'Debian',
-      :lsbdistid       => 'Ubuntu',
-      :operatingsystem => 'Ubuntu',
-      :lsbdistcodename => 'precise',
-      :architecture    => 'amd64',
-    }
-  }
-  let(:pre_condition) { 'include fluentd' }
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let :facts do
+        facts
+      end
 
-  context 'gem plugin' do
-    let(:title) { 'fluent-plugin-elasticsearch' }
-    let(:params) do
-      {
-          type: 'gem'
-      }
-    end
+      let(:pre_condition) { 'include fluentd' }
 
-    it do
-      should contain_package('fluent-plugin-elasticsearch').with({
-        'ensure'   => 'present',
-        'provider' => 'fluentd_gem',
-        'notify'   => 'Class[Fluentd::Service]'
-      })
-    end
-  end
+      describe 'gem plugin' do
+        let(:title) { 'fluent-plugin-elasticsearch' }
+        let(:params) do
+          {
+              type: 'gem'
+          }
+        end
 
-  context 'file plugin' do
-    let(:title) { 'fluent-plugin-test' }
-    let(:params) do
-      {
-          type: 'file',
-          source: 'puppet:///path/to/source'
-      }
-    end
+        it do
+          should contain_package('fluent-plugin-elasticsearch').with({
+            'ensure'   => 'present',
+            'provider' => 'fluentd_gem',
+            'notify'   => 'Class[Fluentd::Service]'
+          })
+        end
+      end
 
-    it do
-      should contain_file('/etc/td-agent/plugin/fluent-plugin-test').with({
-        'ensure' => 'present',
-        'owner'  => 'td-agent',
-        'group'  => 'td-agent',
-        'mode'   => '0640',
-        'source' => 'puppet:///path/to/source',
-        'notify' => 'Class[Fluentd::Service]'
-      })
+      describe 'file plugin' do
+        let(:title) { 'fluent-plugin-test' }
+        let(:params) do
+          {
+              type: 'file',
+              source: 'puppet:///path/to/source'
+          }
+        end
+
+        it do
+          should contain_file('/etc/td-agent/plugin/fluent-plugin-test').with({
+            'ensure' => 'present',
+            'owner'  => 'td-agent',
+            'group'  => 'td-agent',
+            'mode'   => '0640',
+            'source' => 'puppet:///path/to/source',
+            'notify' => 'Class[Fluentd::Service]'
+          })
+        end
+      end
     end
   end
 end
